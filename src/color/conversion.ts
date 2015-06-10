941d28dc27b0e2a59df69f6c499bfa8286dce427
Igor <igor.bezkrovny@transgaming.com>
2015-06-11 00:06:33 +0300

    rgb2xyz & xyz2lab fixed

--- src/color/conversion.ts
+++ src/color/conversion.ts
@@ -22,54 +22,79 @@ module IQ.Color {
 			g = g / 255;        //G from 0 to 255
 			b = b / 255;        //B from 0 to 255
 
+/*
+			TODO: What is this?
 			r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
 			g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
 			b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+*/
 
+/*
+			TODO: this will be removed because it is not by wiki: https://en.wikipedia.org/wiki/Lab_color_space and https://github.com/baldmountain/Squelch/blob/master/noiseimageunit/noiseimageunitFilterKernel.cikernel
 			r = r * 100;
 			g = g * 100;
 			b = b * 100;
-
+*/
 			//Observer. = 2°, Illuminant = D65
 			return {
 				x : r * 0.4124 + g * 0.3576 + b * 0.1805, y : r * 0.2126 + g * 0.7152 + b * 0.0722, z : r * 0.0193 + g * 0.1192 + b * 0.9505
 			}
 		}
 
+		// TODO: fix like rgb2xyz fixed
 		static xyz2rgb(x : number, y : number, z : number) : { r : number; g : number; b : number } {
+/*
+			TODO: this will be removed because it is not by wiki: https://en.wikipedia.org/wiki/Lab_color_space and https://github.com/baldmountain/Squelch/blob/master/noiseimageunit/noiseimageunitFilterKernel.cikernel
 			x = x / 100;        //X from 0 to  95.047      (Observer = 2°, Illuminant = D65)
 			y = y / 100;        //Y from 0 to 100.000
 			z = z / 100;        //Z from 0 to 108.883
+*/
 
 			var r = x * 3.2406 + y * -1.5372 + z * -0.4986, g = x * -0.9689 + y * 1.8758 + z * 0.0415, b = x * 0.0557 + y * -0.2040 + z * 1.0570;
 
+/*
+			// TODO: what is this?
 			r = r > 0.0031308 ? 1.055 * Math.pow(r, 1 / 2.4) - 0.055 : 12.92 * r;
 			g = g > 0.0031308 ? 1.055 * Math.pow(g, 1 / 2.4) - 0.055 : 12.92 * g;
 			b = b > 0.0031308 ? 1.055 * Math.pow(b, 1 / 2.4) - 0.055 : 12.92 * b;
+*/
 
 			return {
 				r : r * 255, g : g * 255, b : b * 255
 			}
 		}
 
-		private static _refX : number = 95.047;
-		private static _refY : number = 100.0;
-		private static _refZ : number = 108.883;
+/*
+ 		TODO: this will be removed because it is not by wiki: https://en.wikipedia.org/wiki/Lab_color_space and https://github.com/baldmountain/Squelch/blob/master/noiseimageunit/noiseimageunitFilterKernel.cikernel
+		private static _refX : number = 95.047; //ref_X =  95.047   Observer= 2°, Illuminant= D65
+		private static _refY : number = 100.00; //ref_Y = 100.000
+		private static _refZ : number = 108.883; //ref_Z = 108.883
+*/
 
+		private static _refX : number = 0.95047; //ref_X =  95.047   Observer= 2°, Illuminant= D65
+		private static _refY : number = 0.10000; //ref_Y = 100.000
+		private static _refZ : number = 1.08883; //ref_Z = 108.883
+
+		private static _xyz2lab_helper(value  : number) : number {
+			return value > 0.008856451679035631 ? Math.pow(value, 1 / 3) : ( 7.787037037037037 * value + 0.13793103448275862);
+		}
 		static xyz2lab(x : number, y : number, z : number) : {L : number, a : number, b : number} {
 			x = x / Conversion._refX;          //ref_X =  95.047   Observer= 2°, Illuminant= D65
 			y = y / Conversion._refY;          //ref_Y = 100.000
 			z = z / Conversion._refZ;          //ref_Z = 108.883
 
-			x = x > 0.008856 ? Math.pow(x, 1 / 3) : ( 7.787 * x ) + ( 16 / 116 );
-			y = y > 0.008856 ? Math.pow(y, 1 / 3) : ( 7.787 * y ) + ( 16 / 116 );
-			z = z > 0.008856 ? Math.pow(z, 1 / 3) : ( 7.787 * z ) + ( 16 / 116 );
+			x = Conversion._xyz2lab_helper(x);
+			y = Conversion._xyz2lab_helper(y);
+			z = Conversion._xyz2lab_helper(z);
 
 			return {
-				L : ( 116 * y ) - 16, a : 500 * ( x - y ), b : 200 * ( y - z )
+				L : ( 116 * y ) - 16,
+				a : 500 * ( x - y ),
+				b : 200 * ( y - z )
 			}
 		}
 
+		// TODO: fix like xyz2lab fixed
 		static lab2xyz(L : number, a : number, b : number) : {x : number, y : number, z : number} {
 			var y = ( L + 16 ) / 116, x = a / 500 + y, z = y - b / 200;
 
